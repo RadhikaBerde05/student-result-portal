3b7fe984c2c6b306db7d9a3c55adcc69e923e886
RadhikaBerde05 <a250035@famt.ac.in>
2026-06-12 15:37:20 +0530

    Premium Dashboard UI

--- script.js
+++ script.js
@@ -1,76 +1,69 @@
 const students = {
 
-    MCA101:{
-        name:"Radhika Berde",
-        percentage:85,
-        status:"PASS",
-        java:90,
-        python:85,
-        dbms:80
-    },
+MCA101:{
+name:"Radhika Berde",
+percentage:85,
+status:"PASS"
+},
 
-    MCA102:{
-        name:"Sneha Patil",
-        percentage:78,
-        status:"PASS",
-        java:75,
-        python:80,
-        dbms:79
-    },
+MCA102:{
+name:"Sneha Patil",
+percentage:78,
+status:"PASS"
+},
 
-    MCA103:{
-        name:"Tanvi More",
-        percentage:35,
-        status:"FAIL",
-        java:40,
-        python:30,
-        dbms:35
-    }
+MCA103:{
+name:"Tanvi More",
+percentage:35,
+status:"FAIL"
+}
 
 };
 
 function checkResult(){
 
-    let rollNo =
-    document.getElementById("rollNo")
-    .value
-    .toUpperCase();
+let rollNo =
+document.getElementById("rollNo")
+.value
+.toUpperCase();
 
-    let student = students[rollNo];
+let student =
+students[rollNo];
 
-    if(!student){
+if(!student){
 
-        alert("Student Not Found");
-        return;
-    }
+alert("Student Not Found");
 
-    document.getElementById("studentName").innerHTML =
-    student.name;
+return;
+}
 
-    document.getElementById("studentRoll").innerHTML =
-    rollNo;
+document.getElementById(
+"studentName"
+).innerHTML =
+student.name;
 
-    document.getElementById("studentPercentage").innerHTML =
-    student.percentage + "%";
+document.getElementById(
+"studentRoll"
+).innerHTML =
+"Roll No : " + rollNo;
 
-    document.getElementById("studentStatus").innerHTML =
-    student.status;
+document.getElementById(
+"studentStatus"
+).innerHTML =
+"Status : " + student.status;
 
-    document.getElementById("javaMarks").innerHTML =
-    student.java;
+document.getElementById(
+"progressBar"
+).style.width =
+student.percentage + "%";
 
-    document.getElementById("pythonMarks").innerHTML =
-    student.python;
+document.getElementById(
+"progressBar"
+).innerHTML =
+student.percentage + "%";
 
-    document.getElementById("dbmsMarks").innerHTML =
-    student.dbms;
-
-    document.getElementById("progressBar").style.width =
-    student.percentage + "%";
-
-    document.getElementById("progressBar").innerHTML =
-    student.percentage + "%";
-
-    document.getElementById("resultCard").style.display =
-    "block";
+document.getElementById(
+"resultCard"
+).style.display =
+"block";
 }
