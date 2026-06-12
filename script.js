cee42288e456ece4140195ae7c88ce43a80134ff
RadhikaBerde05 <a250035@famt.ac.in>
2026-06-12 15:29:10 +0530

    Professional Dashboard UI

--- script.js
+++ script.js
@@ -1,15 +1,76 @@
-function showResult()
-{
-    let roll=document.getElementById("roll").value;
+const students = {
 
-    if(roll=="101")
-    {
-        document.getElementById("result").innerHTML=
-        "Result: PASS <br> Percentage: 85%";
+    MCA101:{
+        name:"Radhika Berde",
+        percentage:85,
+        status:"PASS",
+        java:90,
+        python:85,
+        dbms:80
+    },
+
+    MCA102:{
+        name:"Sneha Patil",
+        percentage:78,
+        status:"PASS",
+        java:75,
+        python:80,
+        dbms:79
+    },
+
+    MCA103:{
+        name:"Tanvi More",
+        percentage:35,
+        status:"FAIL",
+        java:40,
+        python:30,
+        dbms:35
     }
-    else
-    {
-        document.getElementById("result").innerHTML=
-        "Student Record Not Found";
+
+};
+
+function checkResult(){
+
+    let rollNo =
+    document.getElementById("rollNo")
+    .value
+    .toUpperCase();
+
+    let student = students[rollNo];
+
+    if(!student){
+
+        alert("Student Not Found");
+        return;
     }
+
+    document.getElementById("studentName").innerHTML =
+    student.name;
+
+    document.getElementById("studentRoll").innerHTML =
+    rollNo;
+
+    document.getElementById("studentPercentage").innerHTML =
+    student.percentage + "%";
+
+    document.getElementById("studentStatus").innerHTML =
+    student.status;
+
+    document.getElementById("javaMarks").innerHTML =
+    student.java;
+
+    document.getElementById("pythonMarks").innerHTML =
+    student.python;
+
+    document.getElementById("dbmsMarks").innerHTML =
+    student.dbms;
+
+    document.getElementById("progressBar").style.width =
+    student.percentage + "%";
+
+    document.getElementById("progressBar").innerHTML =
+    student.percentage + "%";
+
+    document.getElementById("resultCard").style.display =
+    "block";
 }
